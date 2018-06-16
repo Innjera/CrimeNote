d54e4ed2abdd417ccbdadc9f6c8b25ab0b826fc1
YuSagawa <yu.sagawa1115.africaunite@gmail.com>
2018-06-16 07:35:58 +0200

    modification

---
+++ app/admin/notes.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Note do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+permit_params :latitude, :longitude, :policeofficer_name, :bribe, :address, :address_city_part, :address_country_part, :situation, :feeling, :currency
+
+end
