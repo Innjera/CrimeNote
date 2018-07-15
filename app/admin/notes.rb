3525fa195b24a009910aa99951e7269be29760e1
yuruby <yu.sagawa1115.africaunite@gmail.com>
2018-07-15 17:27:52 +0200

    modification

--- app/admin/notes.rb
+++ app/admin/notes.rb
@@ -12,6 +12,6 @@ ActiveAdmin.register Note do
 #   permitted
 # end
 
-permit_params :latitude, :longitude, :crime_category, :address_city_part, :address_country_part, :address, :situation, :occured_at_date_part, :occured_at_time_part
+permit_params :latitude, :longitude, :crime_category, :address_city_part, :address_country_part, :address, :situation, :occured_at, :occured_at_date_part, :occured_at_time_part
 
 end
