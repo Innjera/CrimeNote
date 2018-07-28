8df7b4f41937b32a81544033c1795648933dd8c3
yuruby <yu.sagawa1115.africaunite@gmail.com>
2018-07-28 20:25:31 +0200

    modification

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,10 +2,10 @@ class TopController < ApplicationController
   def top
     @notes = Note.order(created_at: :desc).limit(4)
     @notes_all = Note.all
-    @hash = Gmaps4rails.build_markers(@notes_all) do |address, marker|
-      marker.lat address.latitude
-      marker.lng address.longitude
-      marker.infowindow address.situation
+    @hash = Gmaps4rails.build_markers(@notes_all) do |note, marker|
+      marker.lat note.latitude
+      marker.lng note.longitude
+      marker.infowindow render_to_string(partial: "shared/infowindow", locals: { note: note })
     end
   end
 
