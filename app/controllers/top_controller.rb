1289186ae11398fea33f3d8f90a5b46357df4ed5
innjera <yu.sagawa1115.africaunite@gmail.com>
2018-07-29 11:06:11 +0200

    order change

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def top
-    @notes = Note.order(created_at: :desc).limit(4)
+    @notes = Note.order(uodated_at: :desc).limit(4)
     @notes_all = Note.all
     @hash = Gmaps4rails.build_markers(@notes_all) do |note, marker|
       marker.lat note.latitude
